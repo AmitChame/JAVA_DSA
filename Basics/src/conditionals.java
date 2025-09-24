fcd95bbf81f6da060e227a29fafa13889f59b62c
Amit Chame <your-email@example.com>
2025-09-24 15:59:22 +0530

    Add Basics folder to main repo

---
+++ Basics/src/conditionals.java
@@ -0,0 +1,12 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int i=10;
+        int j=11;
+        if(i<j){
+            System.out.println(j +" is greater");
+        }
+        else{
+            System.out.println(j +"is smaller");
+        }
+    }
+}
